b6eda205f2b65fb4d025688dbd9931fadb64f1f7
markiantorno <markiantorno@gmail.com>
2018-02-26 20:53:32 -0500

    done for tonight, will continue tomorrow.

--- src/main/groovy/com/iantorno/xylographer/Xylographer.groovy
+++ src/main/groovy/com/iantorno/xylographer/Xylographer.groovy
@@ -1,18 +1,25 @@
 package com.iantorno.xylographer
 
+import com.iantorno.xylographer.publisher.PublishTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+
+import java.util.regex.Matcher
 
 class Xylographer implements Plugin<Project> {
     @Override
     void apply(Project project) {
         // the meat and potatoes of the plugin
         println "Xylographer has been configured for build versioning!"
-    }
 
-    @Override
-    Object invokeMethod(String s, Object o) {
-        return super.invokeMethod(s, o)
+        project.task('renameAppVersionName', type: PublishTask)
+
+        /*
+         * Since Android build includes versionName info in the app, we must define task dependency in order trigger it
+         * before Android build starts. There is a gradle task named preBuild which is perfect to accomplish this
+         * requirement.
+         */
+        project.tasks.getByName('preBuild').dependsOn('renameAppVersionName')
     }
 }
 
--- src/main/groovy/com/iantorno/xylographer/publisher/PublishTask.groovy
+++ src/main/groovy/com/iantorno/xylographer/publisher/PublishTask.groovy
@@ -6,6 +6,8 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 
+import java.util.regex.Matcher
+
 /**
  * A custom task type, allows projects to create tasks of type 'PublishTask'
  * Reference:
@@ -28,11 +30,58 @@ class PublishTask extends DefaultTask {
 
         println "${message}, ${target}!"
 
-        def extension = project.getExtensions().getByName('android')
-        extension.conventionMapping.versionName = "1.9.0"
-
+        project.configure(project) {
+            // Check if plugin works on an Android module
+            if (it.hasProperty("android")) {
+                // Iterate over app build variants (build types + flavors)
+                project.android.applicationVariants.all { variant ->
+                    // Only change debug build type variants
+                    if (variant.buildType.name == project.android.buildTypes.debug.name) {
+                        // Rename versionName
+                        def customVersionName = variant.mergedFlavor.versionName
+                        println "version name: ${customVersionName}"
+                        variant.mergedFlavor.versionName = customVersionName + " custom"
+                        println "version name combined: ${variant.mergedFlavor.versionName}"
+                    }
+                }
+            }
+        }
     }
 
+    static def renameDebugAppVersionName(variant) {
+        def customVersionName = variant.mergedFlavor.versionName + getCurrentBranchCodeName()
+        variant.mergedFlavor.versionName = customVersionName
+        println "${variant.name} version name: ${customVersionName}"
+    }
+
+    static def getCurrentBranchCodeName() {
+        def currentBranchName = 'git rev-parse --abbrev-ref HEAD'.execute().text.trim()
+        String branchTicketCode = "";
+
+        /*
+         * REGEX For branch naming:
+         *
+         * We want to match our JIRA ticketing pattern, ie: 'BANTA-111', BA-29, ICC-2034'...
+         *
+         * This regex will parse the string for any instance of a 2-5 capital letter identifier, paired with a number
+         * from 1-99999, separated by a '-' character. This is unique for my job's purposes, but could be reformatted to
+         * suit different needs.
+         *
+         *  ^ asserts position at start of the string
+         *  Match a single character present in the list below [A-Z]{2,5}
+         *  {2,5} Quantifier — Matches between 2 and 5 times, as many times as possible, giving back as needed (greedy)
+         *  A-Z a single character in the range between A (index 65) and Z (index 90) (case sensitive)
+         *  \- matches the character - literally (case sensitive)
+         *  \d{1,5} matches a digit (equal to [0-9])
+         *  {1,5} Quantifier — Matches between 1 and 5 times, as many times as possible, giving back as needed (greedy)
+         *  $ asserts position at the end of the string, or before the line terminator right at the end of the string (if any)
+         */
+        Matcher matcher = currentBranchName =~ /^[A-Z]{2,5}\-\d{1,5}$/
+        if (matcher.size() > 0) {
+            branchTicketCode = matcher[0][1]
+        }
+        return branchTicketCode
+    }
 
     /**
      * Builds an Android version code from the version of the project.
